f85d13f8b84d6491828c22bf13c459ea6c4c1306
najmi-smile <tahiranjamraza@gmail.com>
2018-07-13 22:56:29 +0100

    Setup the basic boiler structur for working directory

---
+++ SimpleWeb/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            
+            throw new NotImplementedException();
+            //return View();
+        }
+       
+    }
+}
